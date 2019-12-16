e627c6db4f65a803d9c5ead0b6463e5cc67d60b9
root <root@localhost.localdomain>
2019-12-16 07:46:41 +0000

    配置

---
+++ app/Http/Controllers/Text/TextController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Text;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TextController extends Controller
+{
+    //
+    public function textxml()
+    {
+        echo "huojiasheng  aaa bbb";
+    }
+}
---
+++ app/Http/Controllers/WX/WxController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\WX;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class WxController extends Controller
+{
+    //
+    public function wechat()
+    {
+        $token = 'qwertyuiopasdfg';       //开发提前设置好的 token
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+        $echostr = $_GET["echostr"];
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+        if( $tmpStr == $signature ){        //验证通过
+            echo $echostr;
+        }else{
+            die("not ok");
+        }
+    }
+}

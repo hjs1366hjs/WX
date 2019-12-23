e8a3df05998b7841e5f51f9f6e2d0b6e74ede6a1
huojiasheng <huojiasheng@localhost.localdomain>
2019-12-23 05:49:01 +0000

    推送事件

--- app/Http/Controllers/WX/WxController.php
+++ app/Http/Controllers/WX/WxController.php
@@ -36,11 +36,15 @@ class WxController extends Controller
     {
         $log_file = 'wx.log';       //public
         //将接收的数据记录到日志文件
-        $xml = file_get_contents("php://input");
-        $data =date('Y-m-d H:i:s') . $xml;
+        $xml_str = file_get_contents("php://input");
+        $data =date('Y-m-d H:i:s') . $xml_str;
         file_put_contents($log_file,$data,FILE_APPEND);
 
-        //$xml_arr = simplexml_load_string($xml_str);
+        //处理xml数据
+        $xml_obj = simplexml_load_string($xml_str);
+
+        //入库  其他逻辑
+
     }
 
     public function getuserinfo(){
